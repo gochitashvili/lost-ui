f5e7375b9ee80495e5b2bbc944275ec47a71abeb
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2025-03-22 13:52:29 +0000

    add file upload blocks

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -5,7 +5,7 @@ import {
 } from "lucide-react";
 import * as React from "react";
 
-import { cn } from "../../lib/utils";
+import { cn } from "@/lib/utils";
 import { Button, buttonVariants } from "./button";
 
 function Pagination({ className, ...props }: React.ComponentProps<"nav">) {
